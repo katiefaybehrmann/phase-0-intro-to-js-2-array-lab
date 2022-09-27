b152f5c7b307557af288892721fa51719ec0e51e
Katie <katiefaybehrmann@gmail.com>
2022-09-27 13:48:53 -0600

    commit tree 44112ee25ff5e03ada024cb7f10d1d26ffd088b4

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+    const newNewCats = [name, ...cats];
+    return newNewCats;
+}
+
+function removeLastCat(){
+    const byeLastCat = cats.slice(0,-1);
+    return byeLastCat;
+}
+
+function removeFirstCat(){
+    const byeFirstCat = cats.slice(1);
+    return byeFirstCat;
+}
